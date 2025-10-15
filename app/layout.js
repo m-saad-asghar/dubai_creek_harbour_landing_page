acbcb14146381a6ce1005a604c68fbc8f1a11143
Muhammad Saad Asghar <m.asghar@MacBook-Air-Shiro.local>
2025-10-15 09:32:16 +0400

    add google tag manager code

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "@/node_modules/react-modal-video/css/modal-video.css"
 import "../public/assets/css/style.css"
 import 'swiper/css'
+import Script from 'next/script';
 // import "swiper/css/navigation"
 import "swiper/css/pagination"
 import 'swiper/css/free-mode';
@@ -14,6 +15,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
          <html lang="en" className={`${dM_Sans.variable}`}>
+            <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9Y6NWBS0WZ"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9Y6NWBS0WZ');
+          `}
+        </Script>
+      </head>
             <body>
                 {children}
                 <Toaster position="top-right" reverseOrder={false} />
