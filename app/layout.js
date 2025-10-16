50f3b158c0d06528de41087d3ea48358f22d8a9a
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-10-16 16:04:37 +0400

    add tag

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,26 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-KXZ1V7HTLS');
         `}
       </Script>
+      {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1176964094380053');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </head>
             <body>
                 {children}
