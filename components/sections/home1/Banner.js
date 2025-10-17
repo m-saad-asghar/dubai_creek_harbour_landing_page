8b60b426d811b4dd0fdf6bde2c34573ee770a2a6
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-10-17 14:20:52 +0400

    change logs

--- components/sections/home1/Banner.js
+++ components/sections/home1/Banner.js
@@ -54,7 +54,7 @@ export default function Banner() {
 
   const payload = {
     fields: {
-      TITLE: `Dubai Creek Harbour Landing Page`,
+      TITLE: `Dubai Creek Harbour EN Landing Page`,
       NAME: formData.name,
       PHONE: [
         {
@@ -71,7 +71,7 @@ export default function Banner() {
       SOURCE_DESCRIPTION: formData.message,
       SOURCE_ID: "WEB",
       ASSIGNED_BY_ID: 25,
-      UF_CRM_1754652292782: "Dubai Creek Harbour Landing Page",
+      UF_CRM_1754652292782: "Dubai Creek Harbour EN Landing Page",
     },
     params: {
       REGISTER_SONET_EVENT: "Y",
